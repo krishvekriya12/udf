d04ed13b838bc461620735d49ec4b59d628fed77
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-05-30 15:49:11 +0530

    Add files via upload

---
+++ udf task-2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int i, num;
+  
+  printf("Enter the number: ");
+  scanf("%d", &num);
+  
+  printf("Numbers Divisible by 3 and 5 Between to %d are: \n", num);
+  for (i = 1; i <= num; i++)
+  {
+    if (i % 3 == 0 && i % 5 == 0)
+	{
+      printf("%d ", i);
+    }
+  }
+  return 0;
+}
